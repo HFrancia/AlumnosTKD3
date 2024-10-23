1c0eae0c44ec2a2dd39c4e64d671b0105abde4de
HFrancia <hgfrancia@hotmail.com>
2024-10-22 20:03:06 -0600

    Actualización de headers

--- app.py
+++ app.py
@@ -641,5 +641,5 @@ def generar_reporte_pedidos_hoy_pdf():
         session.close()
 ##Fin --> declaración para ejecución de app.py
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    #port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0')
